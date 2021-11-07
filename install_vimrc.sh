4087abe857ffe2c186c9044a7060a18b50182b7b
rox <seellk@gmail.com>
2021-11-08 00:14:41 +0800

    save 2021/11/08 00:14:59 add neobundle

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 set -e
 
+VIMRCS=~/vim_runtime/vimrcs
 
-cat ~/.vim_runtime/vimrcs/.vimrc >> ~/.vimrc
+cat $VIMRCS/.vimrc >> ~/.vimrc
 # make sure
 vi ~/.vimrc
 
 # copy vimrcs
 mkdir -p ~/.vim/vimrcs/
-cp ~/.vim_runtime/vimrcs/* ~/.vim/vimrcs/
+cp $VIMRCS/* ~/.vim/vimrcs/
 
 # plugin management
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+
+ehco "Execute :NeoBundleInstall(!) in vim to install plugins"
+
