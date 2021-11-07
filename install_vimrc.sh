d908b046677e1fec38820336ae55c09162b37931
rox <seellk@gmail.com>
2021-11-07 23:56:31 +0800

    save in 2021/11/07 23:56:35

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 set -e
 
-cat ~/.vim_runtime/vimrcs/basic_vimrc >> ~/.vimrc
+
+cat ~/.vim_runtime/vimrcs/.vimrc >> ~/.vimrc
+# make sure
+vi ~/.vimrc
+
+# copy vimrcs
+mkdir -p ~/.vim/vimrcs/
 cp ~/.vim_runtime/vimrcs/* ~/.vim/vimrcs/
+
+# plugin management
+mkdir -p ~/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
